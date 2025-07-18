104968336f8fa7afdfb70c8f2b526dbfaeb6c8c2
Felipe Santos <felipesantosmarcelino2004@gmail.com>
2025-07-18 20:17:29 -0300

    build: remove unused import

--- src/pages/MyBookingsPage.tsx
+++ src/pages/MyBookingsPage.tsx
@@ -33,7 +33,7 @@ import {
   Scissors,
   User,
 } from "lucide-react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 // Supondo uma tipagem para o agendamento populado
 interface PopulatedBooking {
